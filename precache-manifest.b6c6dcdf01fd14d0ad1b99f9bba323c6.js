99c88df843fe56a5f4b2679fd8e0c947e2552ba3
muhammadfalahit <muhammadit96@gmail.com>
2020-03-07 18:15:37 +0300

    Updates

---
+++ precache-manifest.b6c6dcdf01fd14d0ad1b99f9bba323c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a5953a16de93b4fc4cdb62ea633b91",
+    "url": "/to-do-react-web/index.html"
+  },
+  {
+    "revision": "c865adaf85e24ec1223b",
+    "url": "/to-do-react-web/static/js/2.dfe7f098.chunk.js"
+  },
+  {
+    "revision": "c71926b7c3a9738c50c3",
+    "url": "/to-do-react-web/static/js/main.0a825815.chunk.js"
+  },
+  {
+    "revision": "e7d3ca31f2149a9c53f2",
+    "url": "/to-do-react-web/static/js/runtime-main.cb736836.js"
+  }
+]);
